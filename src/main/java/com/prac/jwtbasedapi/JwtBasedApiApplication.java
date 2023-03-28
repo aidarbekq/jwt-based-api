e455373e1041667483ec8f569739e7a051c7e3f7
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-28 20:47:55 +0600

    add proj

---
+++ src/main/java/com/prac/jwtbasedapi/JwtBasedApiApplication.java
@@ -0,0 +1,13 @@
+package com.prac.jwtbasedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBasedApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtBasedApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prac/jwtbasedapi/JwtBasedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.jwtbasedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBasedApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
