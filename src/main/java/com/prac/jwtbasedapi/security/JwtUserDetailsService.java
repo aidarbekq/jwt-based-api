7e80500a1c392c68c6cbd47a31269208948f273a
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-29 18:06:54 +0600

    add jwt userdetailsservice

---
+++ src/main/java/com/prac/jwtbasedapi/security/JwtUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.prac.jwtbasedapi.security;
+
+import com.prac.jwtbasedapi.model.User;
+import com.prac.jwtbasedapi.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public JwtUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return null;
+    }
+}
