c5e7bbc3367c31b6443acba8fd60e0b9cf623f84
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-04-04 17:15:04 +0600

    add app config and security config

---
+++ src/main/java/com/prac/jwtbasedapi/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.prac.jwtbasedapi.config;
+
+import com.prac.jwtbasedapi.security.JwtUserDetailsService;
+import com.prac.jwtbasedapi.security.jwt.JwtTokenFilter;
+import com.prac.jwtbasedapi.security.jwt.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+}
