8b0fb80e7760dad316ce2715fae0321205d85bbb
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-04-04 17:23:47 +0600

    modify
    add annotation

--- src/main/java/com/prac/jwtbasedapi/JwtBasedApiApplication.java
+++ src/main/java/com/prac/jwtbasedapi/JwtBasedApiApplication.java
@@ -2,8 +2,10 @@ package com.prac.jwtbasedapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JwtBasedApiApplication {
 
     public static void main(String[] args) {
