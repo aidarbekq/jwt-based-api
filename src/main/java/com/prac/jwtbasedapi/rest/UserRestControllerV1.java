20c56b611a1c639dfd184254ecde65433f4c78d7
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-04-05 13:19:29 +0600

    add User controller

--- src/main/java/com/prac/jwtbasedapi/rest/UserRestControllerV1.java
+++ src/main/java/com/prac/jwtbasedapi/rest/UserRestControllerV1.java
@@ -1,6 +1,6 @@
 package com.prac.jwtbasedapi.rest;
 
-import com.prac.jwtbasedapi.dto.AdminUserDto;
+import com.prac.jwtbasedapi.dto.UserDto;
 import com.prac.jwtbasedapi.model.User;
 import com.prac.jwtbasedapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,23 +11,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping(value = "/api/v1/admin")
-public class AdminRestControllerV1 {
+@RequestMapping(value = "/api/v1/users")
+public class UserRestControllerV1 {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping(value = "/users/{id}")
-    public ResponseEntity<AdminUserDto> getUserById(@PathVariable(name = "id") Long id) {
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Long id) {
         User user = userService.findById(id);
 
         if (user == null) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
 
-        AdminUserDto adminUserDto = AdminUserDto.fromAdminUser(user);
-        return new ResponseEntity<>(adminUserDto, HttpStatus.OK);
+        UserDto userDto = UserDto.fromUser(user);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
     }
 
 }
